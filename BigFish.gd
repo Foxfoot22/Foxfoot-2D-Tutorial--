a0fb103cff93d54187c3c0d4a5d5a48d1c826e0e
Foxfoot22 <107946126+Foxfoot22@users.noreply.github.com>
2022-09-13 12:30:04 -0700

    Changed the fish timer

--- BigFish.gd
+++ BigFish.gd
@@ -2,23 +2,25 @@ extends RigidBody2D
 
 
 signal fish_bite
-var bite_signal = emit_signal("fish_bite")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	
+func _on_BigFish_tree_entered():
 	var target = get_parent()
+# warning-ignore:return_value_discarded
 	self.connect("fish_bite", target, "on_fish_bite")
 	
 	if is_connected("fish_bite", target, "on_fish_bite"):
 		print("connected")
+	
+func _ready():
+	pass
+	
 		
 
 func _on_RigidBody2D_body_entered(body):
 	print("body entered")
-	body.queue_free()
 	##get_tree().call_group("Main", "on_fish_bite")
 	emit_signal("fish_bite")
+	body.queue_free()
 	
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free() 
--- Main.gd
+++ Main.gd
@@ -10,9 +10,7 @@ var score
 var value
 
 signal starting_health
-##signal fish_bite
 
-# Called when the node enters the scene tree for the first time.
 
 func _ready():
 	randomize()
